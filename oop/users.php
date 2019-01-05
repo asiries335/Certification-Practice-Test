0666bb541b6cf2957a78bda590640064154a55e7
sergey <asiries335@gmail.com>
2019-01-05 11:28:20 +0300

    OOP-module: done laboratory work 1.2, exercise 1

--- oop/users.php
+++ oop/users.php
@@ -6,26 +6,22 @@ class User {
     public $login = null;
     public $password = null;
 
+    public function __construct($name, $login, $password)
+    {
+        $this->name = $name;
+        $this->login = $login;
+        $this->password = $password;
+    }
+
     public function showInfo()
     {
         return "name - {$this->name}, login - {$this->login}, password - {$this->password}";
     }
 }
 
-$user0 = new User();
-$user0->name = "john";
-$user0->login = "login_login";
-$user0->password = "erfewrewrwe";
-
-$user1 = new User();
-$user1->name = "sergey";
-$user1->login = "login_sergey";
-$user1->password = "erfewrewrwe11";
-
-$user2 = new User();
-$user2->name = "ivan";
-$user2->login = "login_ivan";
-$user2->password = "erfewrewrwe33";
+$user0 = new User("john", "login_login", "erfewrewrwe");
+$user1 = new User("sergey", "login_sergey", "erfewrewrwe11");
+$user2 = new User("ivan", "login_ivan", "erfewrewrwe33");
 
 echo $user0->showInfo();
 echo $user1->showInfo();
