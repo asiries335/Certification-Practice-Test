1034dee8e701f986834a4a7354fd0bdd0eeda4ab
sergey <asiries335@gmail.com>
2019-01-05 14:00:06 +0300

    OOP-module: done laboratory work 1.4

--- oop/users.php
+++ oop/users.php
@@ -5,12 +5,19 @@ class User {
     public $name = null;
     public $login = null;
     public $password = null;
+    public static $countUser = 0;
 
     public function __construct($name, $login, $password)
     {
         $this->name = $name;
         $this->login = $login;
         $this->password = $password;
+        ++self::$countUser;
+    }
+
+    public function __clone()
+    {
+        ++self::$countUser;
     }
 
     public function __destruct()
@@ -25,12 +32,16 @@ class User {
 }
 
 class SuperUser extends User {
+
     public $role = null;
+    public static $countUserAdmin = 0;
 
     public function __construct($name, $login, $password, $role)
     {
         parent::__construct($name, $login, $password);
         $this->role = $role;
+        ++self::$countUserAdmin;
+        --parent::$countUser;
     }
 
     public function showInfo()
@@ -44,9 +55,12 @@ $user0 = new User("john", "login_login", "erfewrewrwe");
 $user1 = new User("sergey", "login_sergey", "erfewrewrwe11");
 $user2 = new User("ivan", "login_ivan", "erfewrewrwe33");
 
-echo $user0->showInfo();
-echo $user1->showInfo();
-echo $user2->showInfo();
+//echo $user0->showInfo();
+//echo $user1->showInfo();
+//echo $user2->showInfo();
 
 $user = new SuperUser("johnSuper", "login_loginSuper", "erfewrewrweSuper", "admin");
-echo $user->showInfo();
+//echo $user->showInfo();
+
+echo 'users - '. User::$countUser . "<br>";
+echo 'admin users - '. SuperUser::$countUserAdmin . "<br>";
