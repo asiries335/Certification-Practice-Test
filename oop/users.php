81cd696a520288e0c5e78d3a62432545060f0d35
sergey <asiries335@gmail.com>
2019-01-05 12:35:30 +0300

    OOP-module: done laboratory work 1.3

--- oop/users.php
+++ oop/users.php
@@ -24,6 +24,22 @@ class User {
     }
 }
 
+class SuperUser extends User {
+    public $role = null;
+
+    public function __construct($name, $login, $password, $role)
+    {
+        parent::__construct($name, $login, $password);
+        $this->role = $role;
+    }
+
+    public function showInfo()
+    {
+        $result = parent::showInfo();
+        return $result. "role - {$this->role}";
+    }
+}
+
 $user0 = new User("john", "login_login", "erfewrewrwe");
 $user1 = new User("sergey", "login_sergey", "erfewrewrwe11");
 $user2 = new User("ivan", "login_ivan", "erfewrewrwe33");
@@ -31,3 +47,6 @@ $user2 = new User("ivan", "login_ivan", "erfewrewrwe33");
 echo $user0->showInfo();
 echo $user1->showInfo();
 echo $user2->showInfo();
+
+$user = new SuperUser("johnSuper", "login_loginSuper", "erfewrewrweSuper", "admin");
+echo $user->showInfo();
